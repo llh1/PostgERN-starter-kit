a243fbf3787e82e5502c690267c50b8b4f3e94c9
mozafar <kabaros@gmail.com>
2019-08-16 21:23:24 +0100

    Move API routes to separate modules
    change the folder structure of the API routes to put each resource into its own file and mount them with Express.Router

--- server/api/index.js
+++ server/api/index.js
@@ -1,30 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
 
-const usersDb = require('../services/database/users')
+const users = require('./users');
+const status = require('./status');
 
-router.get('/status', (req, res) => {
-  res.send('All good')
-})
-
-/**
- * This route is protected. If you GET /api/protected-status without a valid token
- * then it will 403 (forbidden). Check the README for information on how to authenticate and get a token
- */
-router.get('/protected-status',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.send('All good');
-  })
-
-router.get('/users', (req, res) => {
-  usersDb.getAllUsers().then(data => {
-    res.send(data)
-  }).catch(err => {
-    console.error(err);
-    res.send(500)
-  })
-})
+router.use('/users', users)
+router.use('/status', status)
 
 module.exports = router
